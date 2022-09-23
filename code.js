e60cbf21f0aa87a9c5cb18b0d21b0a9262cf17ab
Ryan Wahle <ryanwahle8@gmail.com>
2022-09-23 12:38:29 -0500

    Added eraser and comments

--- code.js
+++ code.js
@@ -1,6 +1,25 @@
+//Declares variables
 let container = document.querySelector(".container");
+let eraser = document.querySelector(".eraser");
+let sizer = document.querySelector(".sizer");
+let clear = document.querySelector(".clear");
+let color = 'black'
 let size = 16;
 
+//adds eraser event and sets color of mouse
+
+eraser.addEventListener('click', function(){
+    if (eraser.textContent == "Eraser: Off"){
+        eraser.textContent = "Eraser: On";
+        color = 'white';
+    } else{
+        eraser.textContent = "Eraser: Off";
+        color = 'black';
+    }
+    setColor()
+})
+
+//Creates new board
 
 function creator(size){
     
@@ -13,26 +32,35 @@ function creator(size){
      }
 }
 
+//Sets Color of Mouse (meaning changes addEventListener of boxes)
+
+function setColor(box){
+    box.addEventListener('mouseover', function(e){
+        if(e.buttons == 1 || e.buttons == 3){
+        box.style.backgroundColor = `${color}`; 
+        }})
+    box.addEventListener('click', function(){
+        box.style.backgroundColor = `${color}`; 
+})}
+
+//Sets Up new clean board
+
 function setup(){
    let boxes = document.querySelectorAll('.box');
 
     boxes.forEach(box => {
         box.style.backgroundColor = 'white';
-        box.addEventListener('mouseover', function(e){
-        if(e.buttons == 1 || e.buttons == 3){
-           box.style.backgroundColor = 'black'; 
-        } 
-        box.addEventListener('click', function(e){
-        box.style.backgroundColor = 'black'; 
-        } )
+        setColor(box);
         
-})})};
+})};
 
+
+//Initial setup upon loading page
 creator(size);
 setup();
 
-let sizer = document.querySelector(".sizer");
 
+//Add event listener to sizer button (prompts to change size of board)
 sizer.addEventListener('click', function(){
     let size = prompt("Enter number between 1 and 100", "16");
     if(isNaN(size) || size > 100 || size < 1){
@@ -47,8 +75,7 @@ sizer.addEventListener('click', function(){
     
 });
 
-let clear = document.querySelector(".clear");
-
+//Clears the board to white
 clear.addEventListener('click', function(){
     setup();
 });
